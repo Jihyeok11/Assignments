37b0f84681a617d166ac5f5ceb1494b15a22597e
안재완 <woah68@gmail.com>
2020-12-22 23:15:54 +0900

    [Edit] person.api

--- api/views.py
+++ api/views.py
@@ -7,21 +7,11 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 # Create your views here.
 
-@api_view(['GET'])
-def tutorial_list(request):
-    queryset = Person.obejcts.all()
+class PersonViewSet(viewsets.ModelViewSet):
+    queryset = Person.objects.all()
     serializer_class = PersonSerializer
 
-    persons_serializer = PersonSerializer(queryset)
-    return JsonResponse(persons_serializer, safe=False)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def tutorial_detail(request, pk):
-    # find tutorial by pk (id)
-    try: 
-        tutorial = Tutorial.objects.get(pk=pk) 
-    except Tutorial.DoesNotExist: 
-        return JsonResponse({'message': 'The tutorial does not exist'}, status=status.HTTP_404_NOT_FOUND) 
+    def ge
  
 # class PersonView(viewsets.ModelViewSet):
 #     queryset = Person.objects.all()
